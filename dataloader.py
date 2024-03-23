bde1c29709293b5a279cca2966407412aef1eaf7
florencia <florencia@dsensetech.com>
2024-03-23 20:30:49 +0100

    DataLoader per patient

--- dataloader.py
+++ dataloader.py
@@ -1,9 +1,31 @@
 import os
 import glob
+import numpy as np
+from os import path
+from pathlib import Path 
 import torch
 from torch.utils.data import Dataset
 from torchvision.io import read_image
 from itertools import chain
+
+def get_age(lymph_df):
+  n = lymph_df.shape[0]
+  ages = np.zeros(n)
+  for i in range(n):
+    date_of_birth = lymph_df.DOB[i]
+    if '/' in date_of_birth:
+      year = date_of_birth.split('/')[-1]
+    elif '-' in date_of_birth:
+      year = date_of_birth.split('-')[-1]
+
+    # We do not have information of the year of the data acquisition so we will assume it was this year. 
+    # It shouldn't be relevant as it is an additive bias.  
+    ages[i] = 2024 - int(year)
+
+  lymph_df_out = lymph_df.drop(columns='DOB') 
+  lymph_df_out.insert(4, 'AGE', ages)
+  
+  return lymph_df_out
 
 class CustomDataset(Dataset):
     def __init__(self, img_dir, annotations, transform=None):
@@ -47,3 +69,52 @@ class CustomDataset(Dataset):
             image = self.transform(image)
         
         return image, PID, label, gender, lymph_count
+
+class PatientDataset(Dataset):
+    """This dataset samples the images and metadata for each subject"""
+
+    def __init__(self, img_dir, data_df, transform=None):
+        """
+        Args:
+            img_dir: (str) path to the images directory.
+            data_df: (DataFrame) list of subjects used.
+            transform: Optional, transformations applied to the tensor
+        """
+        self.img_dir = img_dir
+        self.transform = transform
+        self.data_df = get_age(data_df)
+
+    def __len__(self):
+        return len(self.data_df)
+
+    def __getitem__(self, idx):
+        """
+        Args:
+            idx: (int) the index of the subject/session whom data is loaded.
+        Returns:
+            sample: (dict) corresponding data described by the following keys:
+                images: (List) List with image tensors
+                label: (int) the diagnosis code
+                id: (str) ID of the participant
+                gender: (str) 'M' or 'F'
+                age: (int) age
+        """
+
+        label = self.data_df.loc[idx, 'LABEL']
+        age = self.data_df.loc[idx, 'AGE']
+        gender = self.data_df.loc[idx, 'GENDER']
+
+
+        id = self.data_df.loc[idx, 'ID']
+        folder_name = path.join(self.img_dir, id)
+        images = []
+        for filename in Path(folder_name).glob('*'):
+          image = read_image(str(filename))
+
+          if self.transform:
+              image = self.transform(image)
+          images.append(image)
+
+        sample = {'images': images, 'label': label,
+                  'id': id, 'gender': gender, 'age': age}
+        return sample
